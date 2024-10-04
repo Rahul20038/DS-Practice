c73da30c20611f14bebf86afca9ef74bf67f1f9c
Rahul20038 <rahulchowdarynallamothu@gmail.com>
2024-10-04 16:40:55 +0530

    Task completed

--- oops3.py
+++ oops3.py
@@ -13,7 +13,7 @@ class Experince:
         if isinstance(new_exp, float) and new_exp > 0:
             self._Exp = new_exp
         else:
-            print("Enetr valid salary")
+            print("Enetr a valid salary")
 rahul_exp = Experince(5)
 print(rahul_exp.Exp)
 rahul_exp.Exp = 6.0
