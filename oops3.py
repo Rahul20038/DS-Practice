e9c0130334aab426404fc82c57220074a7518aa9
Rahul20038 <rahulchowdarynallamothu@gmail.com>
2024-10-04 15:52:58 +0530

    Task completed

---
+++ oops3.py
@@ -0,0 +1,20 @@
+class Experince:
+    """ Encapsulation get & set & property"""
+    def __init__ (self, Exp):
+        self._Exp = Exp
+    
+    @property
+    def Exp(self):
+        print("execute getter")
+        return self._Exp
+    @Exp.setter
+    def Exp(self, new_exp):
+        print("Execute setter")
+        if isinstance(new_exp, float) and new_exp > 0:
+            self._Exp = new_exp
+        else:
+            print("Enetr valid salary")
+rahul_exp = Experince(5)
+print(rahul_exp.Exp)
+rahul_exp.Exp = 6.0
+print(rahul_exp.Exp)
