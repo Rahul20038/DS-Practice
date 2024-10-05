e1a18660ce7a257a41617e5803b3776d771ccfe0
Rahul20038 <rahulchowdarynallamothu@gmail.com>
2024-10-05 19:19:22 +0530

    Task completed

--- 5thoct/oops4.py
+++ 5thoct/oops4.py
@@ -1,4 +1,4 @@
-class Employee():
+"""class Employee():
     def __init__(self, empname, empsalary):
         self.empname = empname
         self.empsalary = empsalary
@@ -7,3 +7,23 @@ class Employee():
         return f"{self.empname} approved loan, {self.empsalary} Salary."
 rahul_emp = Employee("Rahul", 65000)
 rahul_emp.Loan()
+"""
+class Animal():
+    def __init__(self, name):
+        self.name = name
+    def eat(self):
+        return f"{self.name} is Eating."
+class Dog(Animal):
+    def sound(self):
+        return f"{self.name} is Barking."
+class Cat(Animal):
+    def sound(self):
+        return f"{self.name} is Meowing"
+    
+dogg = Dog("tommy")
+catt = Cat("pussy")
+
+print(dogg.sound())
+print(dogg.eat())
+print(catt.sound())
+print(catt.eat())
